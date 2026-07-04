9870d00970d272d135639cdeee1d282e097f16f5
Stellar Developer <developer@stellar.org>
2026-07-04 14:46:20 +0530

    docs: add entrypoint comment to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Render the React application into the root DOM node
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
